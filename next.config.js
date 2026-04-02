29f5b9b48de105af8f528b963cae253c2b6e217c
Champvasu <tanishqfarkya66@gmail.com>
2026-04-02 20:20:49 +0530

    clean deploy setup

--- next.config.js
+++ next.config.js
@@ -1,25 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-
-
-  images: {
-    domains: [],
-  },
-
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
-          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
